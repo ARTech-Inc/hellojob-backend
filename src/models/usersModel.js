907e0bb9ddc5b55512dd59bd7af34655a253d80a
Maulana Rheza <103730296+MaulanaRhezaArdiansyah@users.noreply.github.com>
2023-02-03 00:52:26 +0800

    rheza: joining table getDetail method

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -57,13 +57,26 @@ const usersModel = {
   },
   getDetail: (id) => {
     return new Promise((resolve, reject) => {
-      db.query(`SELECT * FROM users WHERE id = '${id}'`, (error, result) => {
-        if (error) {
-          return reject(error.message);
-        } else {
-          return resolve(result.rows[0]);
+      db.query(
+        `SELECT
+        usr.id, usr.name, usr.email, usr.phone, usr.password, usr.domisili, usr.job_desk, usr.job_status, usr.description, usr.perusahaan, usr.bidang_perusahaan, usr.akun_instagram, usr.akun_linkedin, usr.akun_github, usr.role,
+        json_agg(row_to_json(usrexp)) work_experiences,
+        json_agg(row_to_json(usrskill)) skills
+        FROM users AS usr
+        LEFT JOIN user_experiences AS usrexp
+        ON usr.id = usrexp.user_id
+        LEFT JOIN user_skills AS usrskill
+        ON usr.id = usrskill.user_id
+        WHERE usr.id = '${id}'
+        GROUP BY usr.id`,
+        (error, result) => {
+          if (error) {
+            return reject(error.message);
+          } else {
+            return resolve(result.rows);
+          }
         }
-      });
+      );
     });
   },
   add: ({
