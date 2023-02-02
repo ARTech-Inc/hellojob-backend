db1c71a50424a3d10f3e1d43ed9dfa7ff28b44ef
Maulana Rheza <103730296+MaulanaRhezaArdiansyah@users.noreply.github.com>
2023-02-03 00:34:59 +0800

    rheza: joining table users and user_skills

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -35,10 +35,13 @@ const usersModel = {
         //   `,
         `SELECT
         usr.id, usr.name, usr.email, usr.phone, usr.password, usr.domisili, usr.job_desk, usr.job_status, usr.description, usr.perusahaan, usr.bidang_perusahaan, usr.akun_instagram, usr.akun_linkedin, usr.akun_github, usr.role,
-        json_agg(row_to_json(usrexp)) work_experiences
+        json_agg(row_to_json(usrexp)) work_experiences,
+        json_agg(row_to_json(usrskill)) skills
         FROM users AS usr
         LEFT JOIN user_experiences AS usrexp
         ON usr.id = usrexp.user_id
+        LEFT JOIN user_skills AS usrskill
+        ON usr.id = usrskill.user_id
         ${search ? `WHERE name ILIKE '%${search}%'` : ""}
         ${catJobStatus ? `WHERE job_status ILIKE '%${catJobStatus}%'` : ""}
         GROUP BY usr.id LIMIT ${limit} OFFSET ${(page - 1) * limit}`,
